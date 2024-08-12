68e12d56907acbe042719a0e04f0d7cd56f58a7d
nesma samir <nesmasamir96@gmail.com>
2024-08-12 05:46:35 +0300

    adding setting score value feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //selecting  html elements
+const gameContainer = document.querySelector(".main-container")
 const player0El = document.getElementById("player-0");
 const player1El = document.getElementById("player-1");
 const globalScore = document.querySelectorAll(".global-score"); //  nodelist of global scors for both players array
@@ -8,11 +9,19 @@ const currentScores = document.querySelectorAll(".current-score"); // list of th
 const diceImgEL = document.querySelector(".dice"); // the image of dice 
 const beginDiceImg = document.querySelector(".begin-dice")
 const rollDiceBtn = document.querySelector(".roll-dice-btn");
-// const holdBtn = document.querySelector(".hold-btn");
 const resetBtn = document.querySelector(".reset-btn");
 const winnerUser = document.createElement("div");
 const celebratAudio = document.querySelector("audio");
+const scoreInput = document.querySelector(".dialog-box input");
+const playBtn = document.querySelector(".play-btn");
+const dialogBox = document.querySelector(".dialog-box");
+const warningMsg = document.querySelector(".warning-message");
+const winingScoreEl = document.querySelector(".score-val")
 winnerUser.textContent = "The Winner";
+
+scoreInput.focus()
+
+
 
 // audio promise
 let playPromise;
@@ -20,6 +29,7 @@ let timeoutSwapTurn;
 
 // variables using in the functionality
 let score = 0;
+let winningScoreVal;
 let activePlayer = 0;
 let accumlatedScore = [0, 0];
 let playing = true;
@@ -47,20 +57,19 @@ function toggleDiceImgState(isHiddenDice, isHiddendiceGif) {
 function swapTurns() {
     timeoutSwapTurn = setTimeout(() => {
         toggleDiceImgState(true, false)
-        changeRollBtnState( "activ", "remove");
+        changeRollBtnState("activ", "remove");
         accumlatedScore[activePlayer] += score;
         globalScore[activePlayer].textContent = accumlatedScore[activePlayer];
-
+        
         // check if the accumlated score is >+10 and get the winner and stopthe game
-        if (accumlatedScore[activePlayer] >= 10) {
+        if (accumlatedScore[activePlayer] >= winningScoreVal) {
             playing = false;
             winnerUser.classList.add("winner-text");
             document.getElementById(`player-${activePlayer}`).classList.remove("active-player");
             document.getElementById(`player-${activePlayer}`).classList.add("winner-player");
             document.getElementById(`player-${activePlayer}`).insertAdjacentElement("afterbegin", winnerUser);
             resetBtn.classList.add('activ-reset-btn');
-            changeRollBtnState( "inactiv", "add");
-            // celebratAudio.play();
+            changeRollBtnState("inactiv", "add");
             playPromise = celebratAudio.play();
             currentScores[activePlayer].textContent = 0;
         }
@@ -72,6 +81,23 @@ function swapTurns() {
     }, 1000);
 
 }
+
+// start game
+playBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    winningScoreVal = Number(scoreInput.value);
+    if (scoreInput.value.startsWith("0") || !winningScoreVal || winningScoreVal <= 1 || !Number.isInteger(winningScoreVal)) {
+        console.log("enter a valid number")
+        warningMsg.textContent = " please enter a valid number greater than 1 "
+        return false
+    }
+    warningMsg.textContent = ""
+    winingScoreEl.textContent = winningScoreVal;
+    dialogBox.classList.add("smooth-hide");
+    gameContainer.classList.remove("blur");
+    document.getElementById(`player-${activePlayer}`).classList.add("active-player");
+    changeRollBtnState("activ", "remove")
+})
 
 // rolling dice 
 rollDiceBtn.addEventListener('click', function () {
@@ -87,7 +113,7 @@ rollDiceBtn.addEventListener('click', function () {
         if (dice !== 1) {
             score += dice;
             currentScores[activePlayer].textContent = score;
-            changeRollBtnState( "inactiv", "add");
+            changeRollBtnState("inactiv", "add");
             swapTurns()
         }
 
@@ -95,7 +121,7 @@ rollDiceBtn.addEventListener('click', function () {
         else {
 
             swapTurns()
-            changeRollBtnState( "inactiv", "add")
+            changeRollBtnState("inactiv", "add")
         }
 
     }
@@ -115,17 +141,20 @@ resetBtn.addEventListener("click", function () {
     }
     resetBtn.classList.remove('activ-reset-btn');
     winnerUser.remove();
-    // reset buttons
-    changeRollBtnState( "activ", "remove")
 
     //1.rest the dice imgs
-    toggleDiceImgState(true, false)
+    toggleDiceImgState(true, false);
+
+    // reset roll button
+    changeRollBtnState("inactiv","add")
+
 
     //2. reset score and global score to 0
     score = 0;
     activePlayer = 0;
     accumlatedScore = [0, 0];
     playing = true;
+    winingScoreEl.textContent="";
 
     //looping current score node list and set the 0 value
     for (let i = 0; i < currentScores.length; i++) {
@@ -138,11 +167,15 @@ resetBtn.addEventListener("click", function () {
     }
 
     //3.change the background to the default value
+    document.getElementById(`player-${activePlayer}`).classList.remove("active-player");
     if (player0El.classList.contains("winner-player") || !player0El.classList.contains("active-player")) {
-        player0El.classList.add("active-player");
         player0El.classList.remove("winner-player");
         player1El.classList.remove("active-player");
         player1El.classList.remove("winner-player");
     }
+    scoreInput.value = "";
+    dialogBox.classList.remove("smooth-hide");
+    gameContainer.classList.add("blur")
+    scoreInput.focus()
 
 })
